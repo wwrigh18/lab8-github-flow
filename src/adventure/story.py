d505e9aca3bfc3275028335204beb1676e6b66cd
William Wright <162603673+wwrigh18@users.noreply.github.com>
2025-10-30 19:07:10 +0000

    Added colors to the formatting of story.py's output

--- src/adventure/story.py
+++ src/adventure/story.py
@@ -1,5 +1,9 @@
 from adventure.utils import read_events_from_file
 import random
+from rich import print
+from rich.console import Console
+
+console = Console()
 
 def step(choice: str, events):
     random_event = random.choice(events)
@@ -9,20 +13,20 @@ def step(choice: str, events):
     elif choice == "right":
         return right_path(random_event)
     else:
-        return "You stand still, unsure what to do. The forest swallows you."
+        return "[i][gray3]You stand still,[/gray3] unsure what to do. [dark_green]The forest swallows you.[/dark_green][/i]"
 
 def left_path(event):
-    return "You walk left. " + event
+    return "[red3][i]You walk left.[/red3] " + event
 
 def right_path(event):
-    return "You walk right. " + event
+    return "[blue3][i]You walk right.[/blue3] " + event
 
 if __name__ == "__main__":
     events = read_events_from_file('events.txt')
 
-    print("You wake up in a dark forest. You can go left or right.")
+    print("[gold3 i]You wake up in a dark forest.[/gold3 i] [b i]You can go left or right.[/b i]")
     while True:
-        choice = input("Which direction do you choose? (left/right/exit): ")
+        choice = console.input("[gray69]Which direction do you choose?[/gray69] ([red3]left[/red3]/[blue3]right[/blue3]/[grey37]exit[/grey37]): ")
         choice = choice.strip().lower()
         if choice == 'exit':
             break
